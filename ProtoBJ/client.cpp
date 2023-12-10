429f735c9b6fd29e45bc68b2cdb3e748c641a086
Brandon <brandon@pve-ubuntu.tailcf134.ts.net>
2023-12-10 11:26:12 -0800

    Added working client/serv from wednesday

--- ProtoBJ/client.cpp
+++ ProtoBJ/client.cpp
@@ -1,18 +1,57 @@
-#include <iostream>
 #include <sys/types.h>    // socket, bind
 #include <sys/socket.h>   // socket, bind, listen, inet_ntoa
 #include <netinet/in.h>   // htonl, htons, inet_ntoa
 #include <arpa/inet.h>    // inet_ntoa
-#include <netdb.h>     // gethostbyname
-#include <unistd.h>    // read, write, close
-#include <strings.h>     // bzero
+#include    <netdb.h>     // gethostbyname
+#include    <unistd.h>    // read, write, close
+#include   <strings.h>     // bzero
 #include <netinet/tcp.h>  // SO_REUSEADDR
 #include <sys/uio.h>      // writev
+#include <iostream>
 #include <string>
 #include "networkingAPI.h"
 
 using namespace std;
 
+void playBlackJack(int clientSd);
+
+int main(int argc, char *argv[]){
+    
+    //initialize server address and file name
+    string server_ip = argv[1];
+    int server_port = atoi(argv[2]);
+
+
+
+    //Create socket attributes
+    struct hostent* host = gethostbyname(server_ip.c_str());
+    sockaddr_in sendSockAddr;
+    bzero((char*)&sendSockAddr, sizeof(sendSockAddr));
+    sendSockAddr.sin_family = AF_INET;
+    sendSockAddr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    sendSockAddr.sin_port = htons(server_port);
+
+    //Create socket and connect to server
+    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+    int connectStatus = connect(clientSd, (sockaddr*) &sendSockAddr, sizeof(sendSockAddr));
+    if (connectStatus < 0){
+        cerr << "Failed to connect to the server" << endl;
+        return 0;
+    }
+
+    cout << "Sucessfully connected." << endl;
+
+    //Create data buffer
+    char databuf[5000];
+
+    
+    playBlackJack(clientSd);
+    
+
+    close(clientSd);
+
+    return 0;
+}
 
 void playBlackJack(int clientSd){
     bool donePlaying = false;
@@ -66,44 +105,4 @@ void playBlackJack(int clientSd){
 
     cout << message << endl;
 
-}
-
-int main(int argc, char *argv[]) {
-
-    //Assigns CL args to vars
-    int server_port = atoi(argv[1]);
-    int bufsize = 1500;
-    char * server_name = argv[2];
-
-    //Server IP
-    struct hostent* host = gethostbyname(server_name);
-
-    //Creates socket address and zeroes out data structures
-    sockaddr_in sendSockAddr;
-    bzero((char*) &sendSockAddr, sizeof(sendSockAddr));
-
-    //Assigns address and port
-    sendSockAddr.sin_family = AF_INET;
-    sendSockAddr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
-    sendSockAddr.sin_port = htons(server_port);
-
-    //Creates socket description
-    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
-
-    //Connects socket
-    int connectStatus = connect(clientSd, (sockaddr*)&sendSockAddr, sizeof(sendSockAddr));
-    if (connectStatus < 0) {
-        cerr << "Failed to connect to the server" << endl;
-    }
-
-    // char databuf[1500] = "Hello";
-
-    // write( clientSd, databuf, bufsize ); 
-
-    // read(clientSd, databuf, sizeof(databuf));
-    // cout << databuf << endl;
-
-    playBlackJack(clientSd);
-
-    close(clientSd);
 }
--- ProtoBJ/server.cpp
+++ ProtoBJ/server.cpp
@@ -1,65 +1,24 @@
-#include <iostream>
 #include <sys/types.h>    // socket, bind
 #include <sys/socket.h>   // socket, bind, listen, inet_ntoa
 #include <netinet/in.h>   // htonl, htons, inet_ntoa
 #include <arpa/inet.h>    // inet_ntoa
-#include <netdb.h>     // gethostbyname
-#include <unistd.h>    // read, write, close
-#include <strings.h>     // bzero
-#include <string.h>     // bzero
+#include    <netdb.h>     // gethostbyname
+#include    <unistd.h>    // read, write, close
+#include   <strings.h>     // bzero
 #include <netinet/tcp.h>  // SO_REUSEADDR
 #include <sys/uio.h>      // writev
-#include <cstdlib>
-#include "networkingAPI.h"
+#include <iostream>
+#include <string>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 #include <array>
 #include <ctime>
 #include <random>
 #include <cassert>
+#include "networkingAPI.h"
 
 using namespace std;
-
-int USERS_CONNECTED = 0;
-
-/**
- * Struct to pass arguments through to the readClient function which is called
- * by the thread thus is limited arguments.
- */
-struct thread_args {
-    int sd;
-};
-
-/**
- * readClient(int sd)
- * 
- * readClient measures the time it takes for the server to read all messages
- * coming from the client. General flow of the function is as follows:
- *  1) Get current time - this is the start time
- *  2) Read incoming msg and track how many times read needed to be called
- *  3) Get current time - this is the end time
- *  4) Calculate elapsed time
- *  5) Sends message to client 
- *  6) Displays elapsed time
-*/
-// void* readClient(void* voidArgs) {
-    
-//     char databuf[1500];
-//     struct thread_args *args = (thread_args*) voidArgs;
-
-//     read(args->sd, databuf, 1500);
-//      cout << args->sd << endl;
-//     //Send count to client
-//     char temp[1500] = "welcome";
-//     strcpy(databuf, temp);
-//     write(args->sd, databuf, sizeof(databuf));
-   
-//     while (1) {
-//     }
-//     //close(args->sd);
-
-//     return NULL;
-// }
 
 enum class CardRank{
     rank_2,
@@ -143,6 +102,58 @@ int getCardValue(const Card& card){
 
         default:
             assert(false && "Should never happen");
+    }
+}
+
+void playBlackjack(const Deck&, vector<int>& playerList);
+
+int main(int argc, char* argv[]){
+    int server_port = atoi(argv[1]);
+
+    int numPlayers = 0;
+    vector<int> playerList;
+
+    //Create socket attributes
+    sockaddr_in acceptSock;
+    bzero((char*) &acceptSock, sizeof(acceptSock));  // zero out the data structure
+    acceptSock.sin_family = AF_INET;   // using IP
+    acceptSock.sin_addr.s_addr = htonl(INADDR_ANY); // listen on any address this computer has
+    acceptSock.sin_port = htons(server_port);  // set the port to listen on
+
+    //Create the socket
+    int serverSd = socket(AF_INET, SOCK_STREAM, 0); // creates a new socket for IP using TCP
+    const int on = 1;
+    setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(int));
+
+    // Bind the socket
+    bind(serverSd, (sockaddr*) &acceptSock, sizeof(acceptSock));
+
+    // Listen on the socket
+    int n = 5;
+    listen(serverSd, n);  // listen on the socket and allow up to n connections to wait.
+    sockaddr_in newsock;   // place to store parameters for the new connection
+    socklen_t newsockSize = sizeof(newsock);
+
+
+    while(numPlayers < 6){
+        int newSd = accept(serverSd, (sockaddr *)&newsock, &newsockSize);
+        playerList.push_back(newSd);
+        numPlayers++;
+    }
+    
+    
+        
+    Deck d = createDeck();
+    shuffleDeck(d);
+    shuffleDeck(d);
+    playBlackjack(d, playerList);
+        
+    
+    
+    
+    
+    for (int i = 0; i < numPlayers; i++){
+        close(playerList[i]);
     }
 }
 
@@ -246,71 +257,3 @@ void playBlackjack(const Deck& deck, vector<int>& playerList)
 
     
 }
-
-/**
- * int main (int argc, char *argv[])
- * 
- * Main driver of the program. The function creates the socket and listens for
- * any connections.  Once a connection is made, a thread is made for that
- * connection so multiple connection can be ran simultaneouslly through
- * multi-threading.
- */
-int main (int argc, char *argv[]) {
-
-    // Create the socket
-    int server_port = atoi(argv[1]); //Sets port to first arg
-
-    //acceptSock is a data structure that stores the server's address and port
-    sockaddr_in acceptSock;
-    bzero((char*) &acceptSock, sizeof(acceptSock));  // zero out the data structure
-    acceptSock.sin_family = AF_INET;   // using IP
-    acceptSock.sin_addr.s_addr = htonl(INADDR_ANY); // listen on any address this computer has
-    acceptSock.sin_port = htons(server_port);  // set the port to listen on
-
-    //serverSd is the actual socket itself
-    int serverSd = socket(AF_INET, SOCK_STREAM, 0); // creates a new socket for IP using TCP
-
-    const int on = 1;
-
-    setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(int));  // this lets us reuse the socket without waiting for hte OS to recycle it
-
-    // Bind the socket
-
-    bind(serverSd, (sockaddr*) &acceptSock, sizeof(acceptSock));  // bind the socket using the parameters we set earlier
-    
-    // Listen on the socket
-    int n = 5;
-    listen(serverSd, n);  // listen on the socket and allow up to n connections to wait.
-
-    // Accept the connection as a new socket
-
-    sockaddr_in newsock;   // place to store parameters for the new connection
-    socklen_t newsockSize = sizeof(newsock);
- 
-    while (1) {
-	    int newSd = accept(serverSd, (sockaddr *)&newsock, &newsockSize);  // grabs the new connection and assigns it a temporary socket
-        
-        //Creates thread and thread_arg struct
-        pthread_t new_thread;
-        struct thread_args *args = new thread_args;
-        args->sd = newSd;
-
-        USERS_CONNECTED++;
-        //pthread_create(&new_thread, NULL, readClient, (void*) args);
-        
-        //A single lobby -- would need to be 2D eventually
-        vector<int> playerList;
-        playerList.push_back(newSd); //add player to lobby
-
-        if (USERS_CONNECTED == 1){
-            Deck d = createDeck();
-            shuffleDeck(d);
-            shuffleDeck(d);
-            playBlackjack(d, playerList);
-        }
-    }
-    return 0;
-
-}
-
-
